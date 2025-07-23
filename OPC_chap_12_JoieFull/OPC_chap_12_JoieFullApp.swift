06478c1e35a73805d83e7b729c1926ddcbad4bf8
Hugues <hugues.bousselet@gmail.com>
2025-07-23 14:38:06 +0200

    Initial Commit

---
+++ OPC_chap_12_JoieFull/OPC_chap_12_JoieFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  OPC_chap_12_JoieFullApp.swift
+//  OPC_chap_12_JoieFull
+//
+//  Created by Hugues BOUSSELET on 23/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct OPC_chap_12_JoieFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
